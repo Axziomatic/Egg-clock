fe0972398f7f42e8083c557f6dd95a1504b1de05
Jim Bothén <jim.bothen@gmail.com>
2025-02-24 10:51:12 +0100

    init commit, added Main and Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="">
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
