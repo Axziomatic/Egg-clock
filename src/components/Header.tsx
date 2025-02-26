e64df2182a211b1dfdf27fe0a02f218d7c5ac4d8
Jim Bothén <jim.bothen@gmail.com>
2025-02-26 12:15:16 +0100

    Added buttons for navigation between TimerPage and Recipes, renamed Main to TimerPage. Installed React Router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 export default function Header() {
   return (
-    <header className="bg-slate-800 flex justify-center">
-      <h1 className="text-2xl py-4 text-yellow-200">Time for Egg</h1>
-    </header>
+    <>
+      <header className="bg-slate-800 flex justify-center">
+        <h1 className="text-2xl py-4 text-yellow-200">Time for Egg</h1>
+      </header>
+      <div className="flex justify-center bg-slate-700">
+        <button className="py-2 px-4  bg-slate-800 text-l text-yellow-300 hover:bg-slate-600">
+          Timer
+        </button>
+        <button className="py-2 px-4 mx-1 bg-slate-800 text-l text-yellow-300 hover:bg-slate-600">
+          Recipes
+        </button>
+      </div>
+    </>
   );
 }
--- src/components/TimerPage.tsx
+++ src/components/TimerPage.tsx
@@ -1,6 +1,6 @@
 import TimerContainer from "./TimerContainer";
 
-export default function Main() {
+export default function TimerPage() {
   return (
     <main className="flex flex-col flex-1 justify-center bg-slate-700">
       <TimerContainer />
