e602cbd1a18615204a33fc73a8e5ca1f86040f1f
Jim Bothén <jim.bothen@gmail.com>
2025-02-26 12:46:40 +0100

    Cleaned up unnecessary code in TimerPage

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <main className="flex flex-col flex-1 justify-center bg-slate-700">
-      <Outlet /> {/* Här visas alla sidor som definieras i <Route> */}
+      <Outlet />
     </main>
   );
 }
--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,9 +1,5 @@
 import TimerContainer from "../components/TimerContainer";
 
 export default function TimerPage() {
-  return (
-    <main className="flex flex-col flex-1 justify-center bg-slate-700">
-      <TimerContainer />
-    </main>
-  );
+  return <TimerContainer />;
 }
